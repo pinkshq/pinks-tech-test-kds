81ad94212edf4d7424ef0a7de6988dc2138afa06
luislucena16 <lucenaluis137@gmail.com>
2025-06-30 18:51:28 +0200

    feat: add global toaster for notifications

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import { OrdersProvider } from "@/contexts/Orders.context";
 import { RidersProvider } from "@/contexts/Riders.context";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <OrdersProvider>
       <RidersProvider>
+        <Toaster />
         <Component {...pageProps} />
       </RidersProvider>
     </OrdersProvider>
