3a3a8183316dfef29b8d2194c2716214fcaa61b3
luislucena16 <lucenaluis137@gmail.com>
2025-06-30 16:07:04 +0200

    feat: implement interactive Kanband board

--- components/Kanban/Kanban.tsx
+++ components/Kanban/Kanban.tsx
@@ -1,23 +1,35 @@
 import s from "./Kanban.module.scss";
 import Column from "../Column";
 import { useOrders } from "@/contexts/Orders.context";
+import { Order } from "@/dtos/Order.dto";
 
 export default function Kanban() {
-  const { orders } = useOrders();
+  const { orders, updateOrderState } = useOrders();
+
+  const handleStateChange = (order: Order) => {
+    if (order.state === "PENDING") {
+      updateOrderState(order.id, "IN_PROGRESS");
+    } else if (order.state === "IN_PROGRESS") {
+      updateOrderState(order.id, "READY");
+    }
+  };
 
   return (
     <section className={s["pk-kanban"]}>
       <Column
         title="Pendiente"
         orders={orders.filter((i) => i.state === "PENDING")}
-        onClick={() =>
-          alert(
-            "mmmmm..., deberias de modificar esto! tenemos que hacer que las ordenes lleguen hasta listo y se entreguen!"
-          )
-        }
+        onClick={handleStateChange}
       />
-      <Column title="En preparación" orders={[]} />
-      <Column title="Listo" orders={[]} />
+      <Column
+        title="En preparación"
+        orders={orders.filter((i) => i.state === "IN_PROGRESS")}
+        onClick={handleStateChange}
+      />
+      <Column
+        title="Listo"
+        orders={orders.filter((i) => i.state === "READY")}
+      />
     </section>
   );
 }
